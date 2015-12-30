4d2f04b5b053f06ed287e7b09c62173785a2b986
iain <iain@soulflyer.co.uk>
2015-12-30 20:21:27 +0700

    open-project and open-project.sh work well together here

--- open-project.sh
+++ open-project.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
-if [ ! -f "/tmp/missing.txt" ]
-then
-    check-images -s /Users/iain/Pictures/Published/large > /tmp/missing.txt
-fi
+# Opens a photo project in finder and aperture. Given an integer parameter
+# will look for the project in /tmp/missing.txt.
+# If there is no parameter or this file is missing it will build the file,
+# then open the project.
+
 nth=1
 
 if [ $1 ]
 then
-   nth=$1
+    nth=$1
+else
+    echo "rebuilding list of projects"
+    check-images -s /Users/iain/Pictures/Published/large > /tmp/missing.txt
 fi
-open-project $(tail -r /tmp/missing.txt | sed -e"$nth"p -n)
+
+if [ ! -f "/tmp/missing.txt" ]
+then
+    echo "project list missing, rebuilding"
+    check-images -s /Users/iain/Pictures/Published/large > /tmp/missing.txt
+fi
+
+if [ -s "/tmp/missing.txt" ]
+   then
+   open-project $(tail -r /tmp/missing.txt | sed -e"$nth"p -n)
+else
+    echo "All projects are ok"
+fi
